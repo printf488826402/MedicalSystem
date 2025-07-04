8904bcb01768fc42b7b1aeb29bcc8dd82f3533cb
printf488826402 <17352720986@163.com>
2025-07-04 20:30:17 +0800

    Initial commit

---
+++ medical/src/main/java/com.qst.medical/domain/superdomain/SuperDomian.java
@@ -0,0 +1,16 @@
+package com.qst.medical.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.io.Serializable;
+
+public class SuperDomain implements Serializable {
+    private Long total; //总记录数
+    @JsonIgnore
+    private Integer pn = 1; //当前页
+    @JsonIgnore
+    private Integer size = Integer.MAX_VALUE; //每页大小
+    @JsonIgnore
+    private String keyword;//查询关键字
+    //省略构造方法以及get/set方法
+}
